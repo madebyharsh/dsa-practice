6c95979cb2bdf822b29963671868bb68b25a7e68
Harsh Devavanshi <madebyharsh@users.noreply.github.com>
2026-04-13 11:04:38 +0530

    GS_String_RunLength

--- src/strings/ST_10_RunLength.java
+++ src/strings/ST_10_RunLength.java
@@ -1,4 +1,40 @@
 package strings;
+/*
+ * Implement a run length encoding function.
+ * For a string input the function returns output encoded as follows:
+ *
+ * "a"     -> "a1"
+ * "aa"    -> "a2"
+ * "aabbb" -> "a2b3"
+ * "aabbbaaabababab"  ->  "a2b3a3b1a1b1a1b1a1b1"
+ */
+
+
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
 
 public class ST_10_RunLength {
+
+    public static String rle(String input) {
+        // Your code goes here
+        StringBuilder sb=  new StringBuilder();
+        input.chars().mapToObj(e->(char) e)
+                .collect(Collectors.groupingBy(e->e, LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .map(e-> "" + e.getKey() + e.getValue()).forEach(e->sb.append(e));
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args)  {
+
+        if("".equals(rle("")) &&
+                "a1".equals(rle("a")) &&
+                "a3".equals(rle("aaa"))){
+            System.out.println("Passed");
+        }else {
+            System.out.println("Failed");
+        }
+    }
 }
+
