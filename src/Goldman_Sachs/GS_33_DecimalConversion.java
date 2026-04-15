de02322ac96f023de9abbacb5027e20f419748fa
Harsh Devavanshi <madebyharsh@users.noreply.github.com>
2026-04-15 11:15:30 +0530

    GS_33_DecimalConversion

--- src/Goldman_Sachs/GS_33_DecimalConversion.java
+++ src/Goldman_Sachs/GS_33_DecimalConversion.java
@@ -1,4 +1,37 @@
 package Goldman_Sachs;
 
+import java.util.HashMap;
+
 public class GS_33_DecimalConversion {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0 || denominator == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        if (numerator < 0 ^ denominator < 0) {
+            sb.append("-");
+        }
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
+        sb.append(num / den);
+        long rem = num % den;
+        if (rem == 0) {
+            return sb.toString();
+        }
+        sb.append(".");
+        HashMap<Long, Integer> seen = new HashMap<>();
+        while (rem != 0) {
+            if (seen.containsKey(rem)) {
+                int ind = seen.get(rem);
+                sb.insert(ind, "(");
+                sb.append(")");
+                return sb.toString();
+            }
+            seen.put(rem, sb.length());
+            rem *= 10;
+            sb.append(rem / den);
+            rem %= den;
+        }
+        return sb.toString();
+    }
 }
