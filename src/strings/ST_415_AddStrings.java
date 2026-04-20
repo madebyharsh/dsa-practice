dd87a5b5bb4b7f4e4ba92b8c85afb9ea9fb9da64
Harsh Devavanshi <madebyharsh@users.noreply.github.com>
2026-04-20 19:21:31 +0530

    ST_415_AddStrings

--- src/strings/ST_415_AddStrings.java
+++ src/strings/ST_415_AddStrings.java
@@ -1,4 +1,24 @@
 package strings;
 
 public class ST_415_AddStrings {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int ind1 = num1.length()-1, ind2 = num2.length()-1;
+        while(ind1 >= 0 || ind2 >=0 || carry != 0){
+            int a = 0, b = 0;
+            if(ind1 >= 0){
+                a = num1.charAt(ind1--) - '0';
+            }
+            if(ind2 >= 0){
+                b = num2.charAt(ind2--) - '0';
+            }
+            System.out.printf("a = %d, b = %d\n", a, b);
+            int sum = a + b + carry;
+            carry = sum / 10;
+            sum %= 10;
+            sb.append(sum);
+        }
+        return sb.reverse().toString();
+    }
 }
