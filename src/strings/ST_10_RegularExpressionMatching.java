54adbc2924bc3073a4d01534e2e51b51e28c10a6
Harsh Devavanshi <madebyharsh@users.noreply.github.com>
2026-04-20 19:21:04 +0530

    ST_10_RegularExpression

--- src/strings/ST_10_RegularExpressionMatching.java
+++ src/strings/ST_10_RegularExpressionMatching.java
@@ -1,4 +1,17 @@
 package strings;
 
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ST_10_RegularExpressionMatching {
+    public static void main(String[] args) {
+        System.out.println(isMatch("ab", "a*"));
+    }
+    public static boolean isMatch(String s, String p) {
+        Pattern pattern = Pattern.compile(p);
+        Matcher matcher = pattern.matcher(s);
+        System.out.println(matcher.matches());
+        return false;
+    }
 }
